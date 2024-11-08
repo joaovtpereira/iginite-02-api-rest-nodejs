32cb76e4f08ba11a06e0a8a091d563309cc4b26e
João Vitor Pereira <joaovitorpereira@MacBook-Pro-de-Joao.local>
2024-11-08 13:27:36 -0300

    config test env file to using in unitary tests

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,6 @@
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { execSync } from 'node:child_process'
+
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -6,9 +8,16 @@ describe('Transactions routes', () => {
   beforeAll(async () => {
     await app.ready()
   })
+
   afterAll(async () => {
     await app.close()
   })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
   it('should be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
